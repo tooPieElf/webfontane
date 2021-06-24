b45c0c3a79b499f389dcc9017a302f82901521cd
tooPieElf <ndimnezechukwudi@gmail.com>
2021-06-24 06:56:21 +0100

    add App.js files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    async componentDidMount() {
+        this.setState({loading: true});
+        const res = await axios.get('https://api.github.com/repositories');
+        this.setState({
+            repos: res.data,
+            loading: false
+        })
+    }
+
+    searchUsers = async (text) => {
+        this.setState({loading: true})
+        const res = await axios.get(`https://api.github.com/search/repositories?q=${text}`)
+        this.setState({
+            repos: res.data.items,
+            loading: false
+        })
+    };
+
+    clearUsers = () => this.setState({repos: [],loading: false});
+
+    setRepo = (svn_url) => this.setState({repoName: svn_url  })
+
+  render() {
+        const {loading, repos, repoName} = this.state
+    return (
+        <Router>
+        <div className = "App">
+          <NavBar title="Webb Fontane"/>
+            <div className="container">
+                <Switch>
+                <Route path = '/' exact  render ={()=>
+                    <>
+                        <Search searchUsers={this.searchUsers} clearUsers = {this.clearUsers} />
+                        <Repositories repos = {repos} loading = {loading} setRepo ={this.setRepo}/>
+                    </>
+
+                }/>
+                <Route path = '/analysis' component={ ()=> <Analysis repoName = {repoName} />}/>
+                </Switch>
+
+
+            </div>
+        </div>
+        </Router>
+    );
+  }
 }
 
 export default App;
